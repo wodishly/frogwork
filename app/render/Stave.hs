702512b0c1256b26391467ec32d2f83ec14f383c
wodishly <klausmbaki@gmail.com>
2025-11-09 10:47:42 -0800

    no more segfault

--- app/render/Stave.hs
+++ app/render/Stave.hs
@@ -1,10 +1,8 @@
-{-# OPTIONS_GHC -Wno-type-defaults #-}
 module Stave where
 
 import Control.Monad (unless)
-import Data.Bifunctor (second)
 
-import Foreign (alloca, peek, peekArray, withArray)
+import Foreign (alloca, peek)
 import Foreign.C (withCString)
 import GHC.Ptr (Ptr (Ptr))
 
@@ -52,11 +50,24 @@ ordeal :: IO FT_Error -> IO ()
 ordeal = (>>= flip unless (error "freetype error") . (== 0))
 
 makeStavebook :: IO FT_Library
-makeStavebook = alloca (ordeal . ft_Init_FreeType' >> peek)
+makeStavebook = alloca $ \pointer -> do
+  putStrLn "making stavebook..."
+  ordeal (ft_Init_FreeType' pointer)
+  putStrLn "stavebook withstood the ordeal `init_freetype`!"
+  p <- peek pointer
+  putStrLn $ "stavebook points to: " ++ show p
+  putStrLn ""
+  return p
 
 makeFeather :: FT_Library -> FilePath -> IO FT_Face
-makeFeather library path = withCString path 
-  (\(Ptr p) -> alloca (ordeal . ft_New_Face' library (Ptr p) 0 >> peek))
+makeFeather library path = withCString path $ \cstring@(Ptr char) -> alloca $ \pointer -> do
+  putStrLn $ "making feather from " ++ show cstring ++ "..."
+  ordeal (ft_New_Face' library (Ptr char) 0 pointer)
+  putStrLn "feather withstood the ordeal `new_face`!"
+  p <- peek pointer
+  putStrLn $ "feather points to: " ++ show p
+  putStrLn ""
+  return p
 
 -- | See [source](https://hackage.haskell.org/package/freetype2-0.2.0/docs/src/FreeType.Core.Types.html#line-139).
 staveShapeName :: FT_Glyph_Format -> String
@@ -94,87 +105,86 @@ loadStave :: FilePath -> Char -> Int -> Int -> IO TextureObject
 loadStave path stave greatness texUnit = do
   -- load the stavebook
   stavebook <- makeStavebook
-  print stavebook
-  print "we made the stavebook"
 
   -- load the feather
   feather <- makeFeather stavebook path
-  print feather
-  print "one"
-  feather' <- peek feather
-  print "two"
   ordeal (ft_Set_Pixel_Sizes' feather (fromIntegral greatness) 0)
-  print "we made the feather"
+  putStrLn "feather withstood the ordeal `set_pixel_sizes`!"
+  feather' <- peek feather
+  putStrLn "feather record peeked!"
+  putStrLn ""
 
   -- load the glyph from the unicodepoint
   finger <- ft_Get_Char_Index feather (fromIntegral $ fromEnum stave)
   ordeal (ft_Load_Glyph' feather finger 0)
-  print finger
-  print "we got the finger"
+  putStrLn "finger withstood the ordeal `load_glyph`!"
+  putStrLn $ "finger is: " ++ show finger
+  putStrLn ""
 
   let slot = frGlyph feather'
+  putStrLn $ "slot is: " ++ show slot
   slot' <- peek slot
+  putStrLn "slot record peeked!"
+  putStrLn ""
 
   let staveTell = frNum_glyphs feather'
-      shape = gsrFormat slot'
+  putStrLn $ "stave tell is: " ++ show staveTell
+  let shape = gsrFormat slot'
+  putStrLn $ "stave shape is: " ++ staveShapeName shape
+  putStrLn ""
 
-  print $ "slot: " ++ show slot
-  print $ "stave tell: " ++ show staveTell
-  print $ "stave shape:" ++ staveShapeName shape
-
-  -- This is [] for Ubuntu Mono, but I'm guessing for bitmap
-  -- fonts this would be populated with the different font sizes.
-  -- putStr "greatness:"
+  -- this is segfaulting
   -- let greatnessTell = frNum_fixed_sizes feather'
   --     sizesPtr = frAvailable_sizes feather'
-  -- sizes <- forM (flight $ pred $ fromIntegral greatnessTell)
+  -- _sizes <- forM [0 .. (fromIntegral greatnessTell)]
   --   (\i -> peek . plusPtr sizesPtr . fromIntegral $ i :: IO FT_Bitmap_Size)
-
-  let (l, t) = doBoth gsrBitmap_left gsrBitmap_top slot'
-  print $ "left: " ++ show l
-  print $ "top: " ++ show t
+  -- putStrLn "greatnesses grabbed!"
 
   -- See [source](https://hackage.haskell.org/package/freetype2-0.2.0/docs/src/FreeType.Core.Base.html#line-705).
   ordeal (ft_Render_Glyph' slot 0)
-  print "we got this far"
+  putStrLn "slot withstood the ordeal `render_glyph`!"
 
   let bitmap = gsrBitmap slot'
-  print $ concat [
-      "width: " ++ show (bWidth bitmap)
-    , " rows: " ++ show (bRows bitmap)
-    , " pitch: " ++ show (bPitch bitmap)
-    , " num_grays: " ++ show (bNum_grays bitmap)
-    , " pixel_mode: " ++ show (bPixel_mode bitmap)
-    , " palette_mode: " ++ show (bPalette_mode bitmap)
-    ]
+      (l, t) = doBoth gsrBitmap_left gsrBitmap_top slot'
+  putStrLn "heres what the bitmap looks like:"
+  putStrLn $ "  lefttop: " ++ show (l, t)
+  putStrLn $ "  width: " ++ show (bWidth bitmap)
+  putStrLn $ "  rows: " ++ show (bRows bitmap)
+  putStrLn $ "  pitch: " ++ show (bPitch bitmap)
+  putStrLn $ "  num_grays: " ++ show (bNum_grays bitmap)
+  putStrLn $ "  pixel_mode: " ++ show (bPixel_mode bitmap)
+  putStrLn $ "  palette_mode: " ++ show (bPalette_mode bitmap)
+  putStrLn ""
 
-  let ((w, w'), (h, h')) = twimap (second fromIntegral . twin . fromIntegral . ($ bitmap)) (bWidth, bRows)
+  let ((w, w'), (_h, h')) = twimap (twin . fromIntegral . ($ bitmap)) (bWidth, bRows)
       pitch = 4 - mod w 4
       nw = fromIntegral (pitch + fromIntegral w')
+  putStrLn "did some reckoning..."
 
-  bitmapData <- peekArray (w*h) (bBuffer bitmap)
-  let data' = addPadding pitch w 0 bitmapData
-  print "we got the… bitmap data?"
+  -- this is segfaulting
+  -- bitmapData <- peekArray (w*h) (bBuffer bitmap)
+  -- putStrLn $ "unpadded bitmap data is: " ++ show bitmapData
+  -- let data' = addPadding pitch w 0 bitmapData
+  -- putStrLn $ "padded bitmap data is: " ++ show data'
 
   GL.texture Texture2D $= Enabled
-  print "hi"
-
   tex <- newBoundTexUnit texUnit
-  print "hihi"
+  putStrLn "made texture!"
 
-  withArray data' $ \pointer -> GL.texImage2D
+  -- withArray data' $ \pointer -> GL.texImage2D
+  GL.texImage2D
     Texture2D
     NoProxy
     0
     R8
     (TextureSize2D nw h')
     0
-    (PixelData Red UnsignedByte pointer)
-  print "hihihi"
+    (PixelData Red UnsignedByte (bBuffer bitmap))
+  putStrLn "did whatever this is!"
 
   GL.textureFilter Texture2D $= ((Linear', Nothing), Linear')
   GL.textureWrapMode Texture2D S $= (Repeated, ClampToEdge)
   GL.textureWrapMode Texture2D T $= (Repeated, ClampToEdge)
-  print "we did it"
+  putStrLn "we did it!"
 
   return tex
